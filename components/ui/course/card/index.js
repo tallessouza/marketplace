2ca0ab44c19868cb9051e4009e6c1fd74257dc86
tallessouza <talles-swd@live.com>
2022-02-28 23:14:27 -0300

    Coursecard padding correction

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -16,7 +16,7 @@ export default function Card({course, disabled, Footer}) {
             alt={course.title}
           />
         </div>
-        <div className="p-8 pb-4 flex-2">
+        <div className="pl-8 pb-4 flex-2">
           <div
             className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
             {course.type}
