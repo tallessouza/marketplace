de46f86e2145bed440d037643531a04609060a20
tallessouza <talles-swd@live.com>
2022-02-28 23:48:15 -0300

    Last ajusts before implement contract

--- components/hooks/useEthPrice.js
+++ components/hooks/useEthPrice.js
@@ -1,21 +1,22 @@
 import useSWR from "swr"
 
 const URL = "https://api.coingecko.com/api/v3/coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=false"
-const COURSE_PRICE = 15
+export const COURSE_PRICE = 15
 
 const fetcher = async url => {
   const res = await fetch(url)
   const json = await res.json()
+
   return json.market_data.current_price.usd ?? null
 }
 
 export const useEthPrice = () => {
-    const {data, ...rest} = useSWR(
-        URL,
-        fetcher,
-        { refreshInterval: 10000 }
-    )
-    const perItem = (data && (COURSE_PRICE / Number(data)).toFixed(6)) ?? null
+  const { data, ...rest } = useSWR(
+    URL,
+    fetcher,
+    { refreshInterval: 10000 }
+  )
 
-    return { eth: {data, perItem, ...rest}}
+  const perItem = (data && (COURSE_PRICE / Number(data)).toFixed(6)) ?? null
+  return { eth: { data, perItem, ...rest}}
 }
