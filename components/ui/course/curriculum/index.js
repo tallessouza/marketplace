d4dcbcfc0991f43c35b1ff85c6be31512f44cbd7
tallessouza <talles-swd@live.com>
2022-03-06 10:56:01 -0300

    Inserting loader on curriculum page

--- components/ui/course/curriculum/index.js
+++ components/ui/course/curriculum/index.js
@@ -1,3 +1,4 @@
+import { Loader } from "@components/ui/common"
 import Link from "next/link"
 
 const lectures = [
@@ -9,7 +10,7 @@ const lectures = [
   "Safe operator",
 ]
 
-export default function Curriculum({locked, courseState}) {
+export default function Curriculum({locked, courseState, isLoading}) {
 
   const statusClass = "px-2 inline-flex text-xs leading-5 font-semibold rounded-full"
   return (
@@ -56,7 +57,8 @@ export default function Curriculum({locked, courseState}) {
                         </span>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        {
+                        { isLoading ?
+                          <Loader/> :
                           locked ?
                             <>
                               { courseState === "deactivated" &&
