e5769c0dd4cfef0fe14b39997a32a9eee3f2d491
tallessouza <talles-swd@live.com>
2022-03-06 10:38:03 -0300

    Detach of courseHash creation to a file

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -20,6 +20,10 @@ export const handler = (web3, provider) => () => {
     web3 ? "web3/network" : null,
     async () => {
       const chainId = await web3.eth.getChainId()
+
+      if (!chainId) {
+        throw new Error("Cannot retreive network, please refresh network!")
+      }
       return NETWORKS[chainId]
     }
   )
