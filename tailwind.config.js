12a97400be3f43073480c1c5f08fa2b2c00f267e
tallessouza <talles-swd@live.com>
2022-03-06 09:38:45 -0300

    Change o deprecated sintaxe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     screens: {
       "xs": "475px",
