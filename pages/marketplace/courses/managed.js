7dd526a51f4b37fc5a3743f403176d74165acb94
tallessouza <talles-swd@live.com>
2022-03-08 23:30:19 -0300

    Activation functionality added

--- pages/marketplace/courses/managed.js
+++ pages/marketplace/courses/managed.js
@@ -32,7 +32,7 @@ const VerificationInput = ({onVerify}) => {
 
 export default function ManagedCourses() {
   const [ proofedOwnership, setProofedOwnership ] = useState({})
-  const { web3 } = useWeb3()
+  const { web3, contract } = useWeb3()
   const { account } = useAdmin({redirectTo: "/marketplace"})
   const { managedCourses } = useManagedCourses(account)
 
@@ -52,6 +52,16 @@ export default function ManagedCourses() {
         ...proofedOwnership,
         [hash]: false
       })
+  }
+
+  const activateCourse = async (courseHash) => {
+    try {
+      await contract.methods.activateCourse(courseHash).send({
+        from: account.data
+      })
+    } catch (e) {
+      console.error(e.message)
+    }
   }
 
   if (!account.isAdmin) {
@@ -88,6 +98,18 @@ export default function ManagedCourses() {
                 <Message type="danger">
                   Wrong Proof!
                 </Message>
+              </div>
+            }
+            { course.state === "purchased" &&
+              <div className="mt-2">
+                <Button 
+                  onClick={() => activateCourse(course.hash)}
+                  variant="green">
+                  Activate
+                </Button>
+                <Button variant="red">
+                  Deactivate
+                </Button>
               </div>
             }
           </ManagedCourseCard>
