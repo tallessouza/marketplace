89e784d205a75fe290c0bffe810ff06104492e57
tallessouza <talles-swd@live.com>
2022-03-08 21:19:18 -0300

    Test creation for smart contract

---
+++ test/courseMarketplace.test.js
@@ -0,0 +1,22 @@
+const CourseMarketplace = artifacts.require("CourseMarketplace")
+
+contract("CourseMarketplace", accounts => {
+    
+    let _contract = null
+    let contractOwner = null
+    let buyer = null
+
+    before(async () => {
+        _contract =await CourseMarketplace.deployed()
+        contractOwner = accounts[0]
+        buyer = accounts[1]
+
+    })
+
+    describe("Purchase the new course", () => {
+
+        it("should resolve into true value", () => {
+            assert(false, "Value is not true")
+        })
+    })
+})
