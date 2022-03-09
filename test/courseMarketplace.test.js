b54201caf619ee85a71b394073c0ab86bb1a1ca1
tallessouza <talles-swd@live.com>
2022-03-08 21:46:25 -0300

    Course data test implemmented

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -2,6 +2,10 @@ const CourseMarketplace = artifacts.require("CourseMarketplace")
 
 contract("CourseMarketplace", accounts => {
     
+    const courseId = "0x00000000000000000000000000003130";
+    const proof = "0x0000000000000000000000000000313000000000000000000000000000003130";
+    const value = "900000000";
+
     let _contract = null
     let contractOwner = null
     let buyer = null
@@ -14,9 +18,36 @@ contract("CourseMarketplace", accounts => {
     })
 
     describe("Purchase the new course", () => {
+        let courseHash
+        before( async () => {
+            await _contract.purchaseCourse(courseId, proof, {
+                from: buyer,
+                value
+            })
+        })
 
-        it("should resolve into true value", () => {
-            assert(false, "Value is not true")
+        it("should get the purchased course hash by index", async () => {
+            const index = 0
+            courseHash = await _contract.getCourseHashAtIndex(index)
+
+            const expectedHash = web3.utils.soliditySha3(
+                { type: "bytes16", value: courseId},
+                { type: "address", value: buyer}
+            )
+
+            assert.equal(courseHash,expectedHash,"Course isn't the same")
+        })
+
+        it("should match the purchased data of the course", async () => {
+            const expectedIndex = 0
+            const expectedState = 0
+            const course = await _contract.getCourseByHash(courseHash)
+
+            assert.equal(course.id, expectedIndex, "Course index should be 0")
+            assert.equal(course.price, value, `Course index should be ${value}`)
+            assert.equal(course.proof, proof, `Course proof should be ${proof}`)
+            assert.equal(course.owner, buyer, `Course buyer should be ${buyer}`)
+            assert.equal(course.state, expectedState, `Course index should be ${expectedState}`)
         })
     })
 })
