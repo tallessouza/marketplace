1fe9fc7d436470d9ee9b2451c5226b23d8a2cd14
tallessouza <talles-swd@live.com>
2022-03-10 20:20:33 -0300

    Button display logic

--- components/providers/web3/hooks/useOwnedCourses.js
+++ components/providers/web3/hooks/useOwnedCourses.js
@@ -23,5 +23,11 @@ export const handler = (web3, contract) => (courses, account) => {
     }
   )
 
-  return swrRes
+  return {
+    ...swrRes,
+    lookup: swrRes.data?.reduce((a, c) => {
+      a[c.id] = c
+      return a
+    },{}) ?? {}
+  }
 }
