5b458bfdce4b30da9e8ad97232b1ca963c96d3e4
tallessouza <talles-swd@live.com>
2022-03-09 23:24:12 -0300

    Gas abstraction and contract balance validation

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -1,8 +1,19 @@
+// Mocha - testing framework
+// Chai - assertion JS library
 const CourseMarketplace = artifacts.require("CourseMarketplace")
 const { catchRevert } = require("./utils/exceptions")
 
 const getBalance = async address => web3.eth.getBalance(address)
 const toBN = value => web3.utils.toBN(value)
+
+const getGas = async result => {
+    const tx = await web3.eth.getTransaction(result.tx)
+    const gasUsed = toBN(result.receipt.gasUsed)
+    const gasPrice = toBN(tx.gasPrice)
+    const gas = gasUsed.mul(gasPrice)
+
+    return gas
+}
 
 contract("CourseMarketplace", accounts => {
 
@@ -159,16 +170,16 @@ contract("CourseMarketplace", accounts => {
 
     it("should be able repurchase with the original buyer", async () => {
       const beforeTxBuyerBalance = await getBalance(buyer)
+      const beforeTxContractBalance = await getBalance(_contract.address)
+      
       const result = await _contract.repurchaseCourse(courseHash2, {from: buyer, value})
-      const tx = await web3.eth.getTransaction(result.tx)
+      
       const afterTxBuyerBalance = await getBalance(buyer)
-
-      const gasUsed = toBN(result.receipt.gasUsed)
-      const gasPrice = toBN(tx.gasPrice)
-      const gas = gasUsed.mul(gasPrice)
+      const afterTxContractBalance = await getBalance(_contract.address)
 
       const course = await _contract.getCourseByHash(courseHash2)
       const exptectedState = 0
+      const gas = await getGas(result)
 
       assert.equal(course.state, exptectedState, "The course is not in purchased state")
       assert.equal(course.price, value, `The course price is not equal to ${value}`)
@@ -177,6 +188,11 @@ contract("CourseMarketplace", accounts => {
         toBN(beforeTxBuyerBalance).sub(toBN(value)).sub(gas).toString(),
         afterTxBuyerBalance,
         "Client balance is not correct!"
+      )
+      assert.equal(
+        toBN(beforeTxContractBalance).add(toBN(value)).toString(),
+        afterTxContractBalance,
+        "Contract balance is not correct!"
       )
     })
 
