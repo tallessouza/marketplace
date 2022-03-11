1772c46b0d38cb0cd3eaf515f7dfe8d1eaa174ae
tallessouza <talles-swd@live.com>
2022-03-11 12:15:46 -0300

    Card rollback

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -1,11 +1,10 @@
 import Image from "next/image"
 import Link from "next/link"
-import { AnimateKeyframes } from "react-simple-animate"
 
-export default function Card({course, disabled, Footer, state}) {
+export default function Card({course, disabled, Footer}) {
   return (
     <div
-      className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      className="bg-gray rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="flex h-full">
         <div className="flex-1 h-full next-image-wrapper">
           <Image
@@ -18,38 +17,11 @@ export default function Card({course, disabled, Footer, state}) {
             priority
           />
         </div>
-        <div className=" flex-2">
-          <div className="flex items-center">
-            <div
-              className="uppercase mr-2 tracking-wide text-sm text-indigo-500 font-semibold">
-              {course.type}
-            </div>
-            <div>
-              { state === "activated" &&
-                <div className="text-xs text-black bg-green-200 p-1 px-3 rounded-full">
-                  Activated
-                </div>
-              }
-              { state === "deactivated" &&
-                <div className="text-xs text-black bg-red-200 p-1 px-3 rounded-full">
-                  Deactivated
-                </div>
-              }
-              { state === "purchased" &&
-                <AnimateKeyframes
-                  play
-                  duration={2}
-                  keyframes={["opacity: 0.2", "opacity: 1"]}
-                  iterationCount="infinite"
-                >
-                  <div className="text-xs text-black bg-yellow-200 p-1 px-3 rounded-full">
-                    Pending
-                  </div>
-                </AnimateKeyframes>
-              }
-            </div>
+        <div className="pl-8 pt-4 flex-2">
+          <div
+            className="pt-4 uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            {course.type}
           </div>
-
           <Link href={`/courses/${course.slug}`}>
             <a
               className="h-12 block mt-1 text-sm sm:text-lg leading-tight font-medium text-black hover:underline">
@@ -61,9 +33,7 @@ export default function Card({course, disabled, Footer, state}) {
             {course.description.substring(0, 70)}...
           </p>
           { Footer &&
-            <div className="mt-2">
-              <Footer />
-            </div>
+            <Footer/>
           }
         </div>
       </div>
