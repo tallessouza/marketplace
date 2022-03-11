18840a7bbbe2994a4fa1f12755463542cc3cb5fc
tallessouza <talles-swd@live.com>
2022-03-11 12:09:41 -0300

    Animation added and buttons rebuild

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -1,10 +1,11 @@
 import Image from "next/image"
 import Link from "next/link"
+import { AnimateKeyframes } from "react-simple-animate"
 
-export default function Card({course, disabled, Footer}) {
+export default function Card({course, disabled, Footer, state}) {
   return (
     <div
-      className="bg-gray rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="flex h-full">
         <div className="flex-1 h-full next-image-wrapper">
           <Image
@@ -14,14 +15,40 @@ export default function Card({course, disabled, Footer}) {
             width="200"
             height="230"
             alt={course.title}
-            priority
           />
         </div>
-        <div className="pl-8 pt-4 flex-2">
-          <div
-            className="pt-4 uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-            {course.type}
+        <div className="p-8 pb-4 flex-2">
+          <div className="flex items-center">
+            <div
+              className="uppercase mr-2 tracking-wide text-sm text-indigo-500 font-semibold">
+              {course.type}
+            </div>
+            <div>
+              { state === "activated" &&
+                <div className="text-xs text-black bg-green-200 p-1 px-3 rounded-full">
+                  Activated
+                </div>
+              }
+              { state === "deactivated" &&
+                <div className="text-xs text-black bg-red-200 p-1 px-3 rounded-full">
+                  Deactivated
+                </div>
+              }
+              { state === "purchased" &&
+                <AnimateKeyframes
+                  play
+                  duration={2}
+                  keyframes={["opacity: 0.2", "opacity: 1"]}
+                  iterationCount="infinite"
+                >
+                  <div className="text-xs text-black bg-yellow-200 p-1 px-3 rounded-full">
+                    Pending
+                  </div>
+                </AnimateKeyframes>
+              }
+            </div>
           </div>
+
           <Link href={`/courses/${course.slug}`}>
             <a
               className="h-12 block mt-1 text-sm sm:text-lg leading-tight font-medium text-black hover:underline">
@@ -33,7 +60,9 @@ export default function Card({course, disabled, Footer}) {
             {course.description.substring(0, 70)}...
           </p>
           { Footer &&
-            <Footer/>
+            <div className="mt-2">
+              <Footer />
+            </div>
           }
         </div>
       </div>
